a036c76172e5a983caf9b56f0ae26ef9abb4efd6
nikhil-tammana <nikhil.ls.tammana@gmail.com>
2019-12-14 23:52:06 -0600

    changed champion auth

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ def champions():
     global summoner_name
 
     # Getting Logged In user.
-    loggedInuser = User.query.filter_by(summoner_name=summoner_name).first()
+    loggedInuser = User.query.filter_by(auth=True).first()
 
     champion_ls = ['Qiyana', 'Wukong', 'Jax', 'Kayn', 'Yuumi', 'Shaco', 'Warwick', 'Xayah', 'Sylas', 'Nidalee', 'Zyra', 'Kled', 'Brand', 'Rammus', 'Illaoi', 'Corki', 'Braum', 'Darius', 'Tryndamere', 'MissFortune', 'Yorick', 'Xerath', 'Sivir', 'Riven', 'Orianna', 'Sejuani', 'Gangplank', 'Malphite', 'Poppy', 'Kaisa', 'Jayce', 'Blitzcrank', 'Trundle', 'Karthus', 'Zoe', 'Graves', 'Lucian', 'Nocturne', 'Lux', 'Shyvana', 'Renekton', 'Fiora', 'Jinx', 'Kalista', 'Fizz', 'Kassadin', 'Sona', 'Vladimir', 'Viktor', 'Rakan', 'Kindred', 'Cassiopeia', 'Maokai', 'Ornn', 'Thresh', 'Kayle', 'Hecarim', 'Khazix', 'Olaf', 'Ziggs', 'Syndra', 'DrMundo', 'Karma', 'Annie', 'Akali', 'Leona', 'Yasuo', 'Kennen', 'Rengar', 'Ryze', 'Shen', 'Zac', 'Pantheon', 'Neeko', 'Bard', 'Sion', 'Vayne', 'Nasus', 'Fiddlesticks', 'TwistedFate', 'Chogath', 'Udyr', 'Morgana', 'Ivern', 'Volibear', 'Caitlyn', 'Anivia', 'Gnar', 'Rumble', 'Zilean', 'Azir', 'Diana', 'Skarner', 'Teemo', 'Urgot', 'Amumu', 'Galio', 'Heimerdinger', 'Ashe', 'Velkoz', 'Singed', 'Taliyah', 'Senna', 'Varus', 'Twitch', 'Garen', 'Nunu', 'MasterYi', 'Pyke', 'Elise', 'Alistar', 'Katarina', 'Ekko', 'Mordekaiser', 'KogMaw', 'Camille', 'Aatrox', 'Draven', 'TahmKench', 'Talon', 'XinZhao', 'Swain', 'AurelionSol', 'LeeSin', 'Aphelios', 'Taric', 'Malzahar', 'Lissandra', 'Tristana', 'RekSai', 'Irelia', 'JarvanIV', 'Nami', 'Jhin', 'Soraka', 'Veigar', 'Janna', 'Nautilus', 'Evelynn', 'Gragas', 'Zed', 'Vi', 'Lulu', 'Ahri', 'Quinn', 'Leblanc', 'Ezreal']
     
